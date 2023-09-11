803b4b6524c8e541300a8ca4649d4d6b93772d2b
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-11 14:14:13 +0200

    complete ex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log('hi');
+/* 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data.
+Confermate lettura come al solito e buon Vue
+*/
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+
+        //Stampo a schermo un messaggio all’interno dell'h1
+        h1Message: 'Hello, this is a mex write with VUE',
+
+        //Bonus - aggiungo img
+        photo: 'https://picsum.photos/536/354'
+      }
+    }
+  }).mount('#app')
